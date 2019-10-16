9abca9b26a28e1be8ba827083cfd3d628ae70ec5
PATHAN <owaiskamal12@gmail.com>
2019-10-16 23:21:27 +0500

    first repo

---
+++ src/main/java/com/example/excelProj/ExcelProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelProjApplication.class, args);
+	}
+
+}
