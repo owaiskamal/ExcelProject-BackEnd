252e3c12860e0221b4f9309ce47d0b40b7798603
huzaifamansoor123 <huzaifamansoor5@gmail.com>
2019-10-19 20:25:47 +0500

    scearch work

--- src/main/java/com/example/excelProj/Service/SearchService.java
+++ src/main/java/com/example/excelProj/Service/SearchService.java
@@ -5,8 +5,11 @@ import com.example.excelProj.Model.UserData;
 import com.example.excelProj.Repository.UserDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -16,7 +19,41 @@ public class SearchService {
 
     public List<UserData> searchAllData(SearchDTO searchDTO){
         List<UserData> userData=userDataRepository.findAll();
+        List<UserData> resultList = new ArrayList<>();
+        resultList.addAll(userData);
 
-        return null;
+        if(!searchDTO.getName().isEmpty()){
+            resultList = resultList.stream()
+                    .filter(p->p.getName().equalsIgnoreCase(searchDTO.getName()))
+                    .collect(Collectors.toList());
+        }
+
+        if(!searchDTO.getEmail().isEmpty()){
+            resultList = resultList.stream()
+                    .filter(p->(p.checkAllEmail(searchDTO.getEmail()))                                )
+                    .collect(Collectors.toList());
+        }
+
+        if(!StringUtils.isEmpty(searchDTO.getStatus())){
+            resultList=resultList.stream()
+                    .filter(data->data.checkAllStatus(searchDTO.getStatus()))
+                    .collect(Collectors.toList());
+        }
+        if (!searchDTO.getPhoneNumber().isEmpty()){
+            resultList=resultList.stream()
+                    .filter(data->data.checkAllPhoneNumber(searchDTO.getPhoneNumber()))
+                    .collect(Collectors.toList());
+
+
+        }
+        if(!searchDTO.getDate().isEmpty()){
+          resultList=resultList.stream()
+                    .filter(data->data.getDateOffc().equalsIgnoreCase(searchDTO.getDate()))
+                    .collect(Collectors.toList());
+        }
+
+
+
+        return resultList;
     }
 }
